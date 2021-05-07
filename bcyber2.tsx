e87bbd24495afac4d610bbf6aea0162ca79dcbae
Liaxim <matthiasramahi@web.de>
2021-05-07 19:33:25 +0200

    Hopsnahme v2

---
+++ bcyber2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="bcyber2" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="bcyber2.png" width="160" height="80"/>
+</tileset>
