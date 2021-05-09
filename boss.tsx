163a18887128cca4ec0aab3b56a20fc066f1e2b6
lia-xim <79609579+lia-xim@users.noreply.github.com>
2021-05-09 20:39:37 +0000

    Deploying to gh-pages from master @ 2f607d322c55438904a7873fc80d98e13349c256 🚀

--- boss.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="boss" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="boss.png" width="96" height="64"/>
-</tileset>
